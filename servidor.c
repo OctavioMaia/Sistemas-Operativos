b54d9923726953ad4f81350ce0a85f469a2a0d51
OctavioMaia <you@example.com>
2016-05-19 22:03:03 +0100

    server update

--- servidor.c
+++ servidor.c
@@ -25,60 +25,60 @@ char* reverse(char* palavra){
    	return palavra;
 }
 
-void backup(char * ficheiro, char* path, char *path_data, char * path_meta,int pid){
-	int fd[2],n, fp_file=0,fp_digest=0,status,f,i,fdCod,xx,a=0,encontrei=0;
-	FILE *fil;
-	char  *string, *aux, divisao[1024], cod[128],codigo[1024],fichiroajuda[1024], *nameFile,cwd[1024],diretFicheiro[1024],dataFicheiro[1024],metaFicheiro[1024];
+void backup(char * ficheiro, char* path, char *path_data, char *path_meta, int pid){
+	int fd[2],n, fp_file=0,fp_digest=0,status,fileDescritor,id,a=0,encontrei=0;
+	FILE *file;
+	char *string, *aux, div[MAX], cod[128],codigo[MAX],fileAtual[MAX], *nomeFicheiro,cwd[MAX],path_ficheiro[MAX],path_dataFicheiro[MAX],path_metaFicheiro[MAX];
 
 	pipe(fd);
 
 	sprintf(codigo,"sha1sum %s",ficheiro);
 	//printf("dadad%s\n",codigo);
-	fil=popen(codigo,"r");
-	fdCod=fileno(fil);
-	read(fdCod,cod,128);
-	close(fdCod);
-	pclose(fil);
+	file=popen(codigo,"r");
+	fileDescritor=fileno(file);
+	read(fileDescritor,cod,128);
+	close(fileDescritor);
+	pclose(file);
 	getcwd(cwd, sizeof(cwd));
 
 	aux=(char *) strdup(cod);
-	nameFile=strsep(&aux," \t");
-	printf("digest: %s\n",nameFile);
+	nomeFicheiro=strsep(&aux," \t");
+	//printf("digest: %s do ficheiro %s\n",nomeFicheiro,ficheiro);
 
-	sprintf(diretFicheiro,"%s/%s",cwd,ficheiro);
-	sprintf(dataFicheiro,"%s/%s",path_data,nameFile);
-	sprintf(metaFicheiro,"%s/%s",path_meta,ficheiro);
+	sprintf(path_ficheiro,"%s/%s",cwd,ficheiro);
+	sprintf(path_dataFicheiro,"%s/%s",path_data,nomeFicheiro);
+	sprintf(path_metaFicheiro,"%s/%s",path_meta,ficheiro);
 
 
-	if(access(diretFicheiro, F_OK) == 0){
-		if(access(metaFicheiro,F_OK) == 0){
-			sprintf(codigo,"ls -l %s",metaFicheiro);
-			fil=popen(codigo,"r");
-			fdCod=fileno(fil);
-			read(fdCod,cod,1024);
-			close(fdCod);
-			pclose(fil);
+	if(access(path_ficheiro, F_OK) == 0){
+		if(access(path_metaFicheiro,F_OK) == 0){
+			sprintf(codigo,"ls -l %s",path_metaFicheiro);
+			file=popen(codigo,"r");
+			fileDescritor=fileno(file);
+			read(fileDescritor,cod,MAX);
+			close(fileDescritor);
+			pclose(file);
 			aux=(char *) strdup(cod);
 			n = strlen(aux)-2;
 
 			while(encontrei==0){
 				if(aux[n]!='/'){
-					divisao[a++]=aux[n--];
+					div[a++]=aux[n--];
 				}else{
 					encontrei=1;
 				}
 			}
-			string = reverse(divisao);
-			if(strcmp(string,nameFile)!=0){
-				sprintf(fichiroajuda,"%s/%s",path_data,nameFile);
-				if(access(dataFicheiro,F_OK) == -1){
-					if(xx=(fork()==0)) {
+			string = reverse(div);
+			if(strcmp(string,nomeFicheiro)!=0){
+				sprintf(fileAtual,"%s/%s",path_data,nomeFicheiro);
+				if(access(path_dataFicheiro,F_OK) == -1){
+					if(id=(fork()==0)) {
 						fp_file = open(ficheiro, O_RDONLY,0664);
 					   
 						dup2(fp_file,0);
 						close(fp_file);
 					   
-						fp_digest = open(fichiroajuda, O_CREAT | O_WRONLY, 0664);
+						fp_digest = open(fileAtual, O_CREAT | O_WRONLY, 0664);
 					
 						dup2(fp_digest, 1);
 						close(fp_digest);
@@ -86,36 +86,36 @@ void backup(char * ficheiro, char* path, char *path_data, char * path_meta,int p
 						execlp("gzip", "gzip", NULL);
 						_exit(1);
 					}else{
-						waitpid(xx,&status,0);
+						waitpid(id,&status,0);
 					}
 				}
-				if(xx=(fork() == 0)) {        	
-					execlp("rm", "rm", metaFicheiro, NULL);
+				if(id=(fork() == 0)) {        	
+					execlp("rm", "rm", path_metaFicheiro, NULL);
 					_exit(1);
 				}else{
-					waitpid(xx,&status,0);
-					if(xx=(fork() == 0)) {        	
-						execlp("ln", "ln", "-s", fichiroajuda,metaFicheiro, NULL);
+					waitpid(id,&status,0);
+					if(id=(fork() == 0)) {        	
+						execlp("ln", "ln", "-s", fileAtual,path_metaFicheiro, NULL);
 						_exit(1);
 					}else{
-						waitpid(xx,&status,0);
+						waitpid(id,&status,0);
 					}
-					printf("%s copiado com sucesso\n",ficheiro); //ficheiro foi mudado
+					printf("%s : copiado com sucesso\n",ficheiro); //ficheiro foi mudado
 				}
 			}else{
-				printf("Já existe um backup deste ficheiro\n");
+				printf("%s : Já existe um backup deste ficheiro\n", ficheiro);
 			}
 		}else{
-			sprintf(fichiroajuda,"%s/%s",path_data,nameFile);
-			if(access(dataFicheiro,F_OK) == -1){
-				sprintf(fichiroajuda,"%s/%s",path_data,nameFile);
-				if(xx=(fork()==0)) {
+			sprintf(fileAtual,"%s/%s",path_data,nomeFicheiro);
+			if(access(path_dataFicheiro,F_OK) == -1){
+				sprintf(fileAtual,"%s/%s",path_data,nomeFicheiro);
+				if(id=(fork()==0)) {
 					fp_file = open(ficheiro, O_RDONLY);
 				   
 					dup2(fp_file,0);
 					close(fp_file);
 				   
-					fp_digest = open(fichiroajuda, O_CREAT | O_WRONLY, 0664);
+					fp_digest = open(fileAtual, O_CREAT | O_WRONLY, 0664);
 				
 					dup2(fp_digest, 1);
 					close(fp_digest);	       
@@ -123,39 +123,42 @@ void backup(char * ficheiro, char* path, char *path_data, char * path_meta,int p
 					execlp("gzip", "gzip", NULL);
 					_exit(1);
 				}else{
-					waitpid(xx,&status,0);
+					waitpid(id,&status,0);
 				}
 			}
-			if(xx=(fork() == 0)) {        	
-				execlp("ln", "ln", "-s", fichiroajuda,metaFicheiro, NULL);
+			if(id=(fork() == 0)) {        	
+				execlp("ln", "ln", "-s", fileAtual,path_metaFicheiro, NULL);
 				_exit(1);
 			}else{
-				waitpid(xx,&status,0);
+				waitpid(id,&status,0);
 			}
 			printf("%s copiado com sucesso\n",ficheiro);
 		}
 	}else{
-		printf("Ficheiro especificado não existe!\n");
+		printf("%s : Ficheiro especificado não existe!\n",ficheiro);
 	}
 }
 
-void restore(char * ficheiro, char * path_meta,int pid){
+void restore(char *ficheiro, char * path_meta, int pid){
 	int fd[2];
-	char metaFicheiro[1024],cwd[1024],fichiroajuda[1024];
-	int fp1,fp2,status,xx;
+	char path_metaFicheiro[MAX],cwd[MAX],fileAtual[MAX];
+	int fp1,fp2,status,id;
 
 	getcwd(cwd, sizeof(cwd));
-	sprintf(metaFicheiro,"%s/%s",path_meta,ficheiro);
+	sprintf(path_metaFicheiro,"%s/%s",path_meta,ficheiro);
 
-	if (access(metaFicheiro, F_OK) != -1) {	  
-		sprintf(fichiroajuda,"%s/%s",cwd,ficheiro);
-		if (xx=(fork()==0)) {
-			fp1 = open(metaFicheiro, O_RDONLY);
+	printf("path_metaFicheiro %s\n",path_metaFicheiro );
+
+	if (access(path_metaFicheiro, F_OK) == 0) {	  
+		sprintf(fileAtual,"%s/%s",cwd,ficheiro);
+		printf("fileAtual %s\n",fileAtual);
+		if (id=(fork()==0)) {
+			fp1 = open(path_metaFicheiro, O_RDONLY);
 		
 			dup2(fp1, 0);
 			close(fp1);
 			   
-			fp2 = open(fichiroajuda, O_CREAT | O_WRONLY, 0664);
+			fp2 = open(fileAtual, O_CREAT | O_WRONLY, 0664);
 			
 			dup2(fp2, 1);
 			close(fp2);
@@ -165,14 +168,12 @@ void restore(char * ficheiro, char * path_meta,int pid){
 			_exit(1);
 		}
 		else {
-			waitpid(xx,&status,0);
-
+			waitpid(id,&status,0);
 			kill(pid, SIGCONT);
 
 			if (WEXITSTATUS(status) == 0) {
 				kill(pid, SIGUSR1);
-			}
-			else {
+			}else {
 				kill(pid, SIGUSR2);
 			}
 		}
@@ -185,8 +186,8 @@ void restore(char * ficheiro, char * path_meta,int pid){
 }
 
 int main(int argc, char* argv[]){
-	char *utilizador, path[MAX], path_data[MAX], path_meta[MAX], ficheiro[MAX], decisao[MAX], path_fifo[MAX], ch, resposta[MAX];
-	int fd=0, pid, i;
+	char *utilizador, path[MAX], path_data[MAX], path_meta[MAX], ficheiro[MAX], decisao[MAX], path_fifo[MAX], ch, resposta[MAX], *ficheiros[MAX];
+	int fd=0, pid, i, j=0, branco=0, aux;
 	struct stat fileStat;
 
 	utilizador = (char *)getenv("USER");
@@ -217,14 +218,48 @@ int main(int argc, char* argv[]){
 
 	pid = atoi(strtok(resposta, " "));
 	strcpy(decisao, strtok(NULL, " "));
-	strcpy(ficheiro, strtok(NULL, " \r\n"));
+	strcpy(ficheiro, strtok(NULL, "\r\n"));
+
+	//printf("ficheiro %s\n",ficheiro );
+
+	while(ficheiro[j]!='\0'){
+		if (ficheiro[j] == ' ') {
+    		branco++;
+		}
+		j++;
+	}
+
+	//printf("tem %d espaços\n",branco );
+	//printf("inseri %s\n",ficheiro);
+
+	aux=branco;
+
+	if(branco==0){
+		ficheiros[0] = strtok(ficheiro," ");
+	}else{
+		ficheiros[0] = strtok(ficheiro," ");
+		i=1;
+		while(branco>0){
+			ficheiros[i] = strtok(NULL," ");
+			branco--;
+			i++;
+		}
+	}
+
+	//for(i=0;i<5;i++) printf("i %s\n",ficheiros[i] );
 
 	if(strcmp(decisao,"backup")==0){
-		backup(ficheiro,path,path_data,path_meta,pid);
+		for(i=0; i <= aux; i++){
+			//printf("entrei for %s\n",ficheiros[i] );
+			backup(ficheiros[i],path,path_data,path_meta,pid);
+		}
 	}else if(strcmp(decisao,"restore")==0){
-		restore(ficheiro,path_meta,pid);
+		for(i=0; i <= aux; i++){
+			printf("%s\n",ficheiros[i] );
+			restore(ficheiros[i],path_meta,pid);
+		}
 	}else{
-		printf("comando invalido\n");
+		printf("Introduza um comando válido.\n");
 	}
 	
 
